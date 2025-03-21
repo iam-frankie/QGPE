8b06ae8cfbf616442b31dad292c9010b49952adb
Frankiewow <57431126+iam-frankie@users.noreply.github.com>
2025-03-21 15:47:00 +0800

    Delete src/main/java/ClusteringKD directory

--- src/main/java/ClusteringKD/Test.java
+++
@@ -1,9 +0,0 @@
-package ClusteringKD;
-
-public class Test {
-    public static void main(String[] args) {
-        double[] a = {1,2,3,4,5};
-        double[] b = {1,2,3,4,5};
-//        System.out.println(CosineSimilarity.cosineSimilarity(a,b));
-    }
-}

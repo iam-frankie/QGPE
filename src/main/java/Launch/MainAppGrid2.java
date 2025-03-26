b02d59d4adf5d443c8e460aff7cfbc3ed86f703d
Frankiewow <57431126+iam-frankie@users.noreply.github.com>
2025-03-27 00:01:09 +0800

    Update MainAppGrid2.java

--- src/main/java/Launch/MainAppGrid2.java
+++ src/main/java/Launch/MainAppGrid2.java
@@ -5,10 +5,6 @@ import Conf.AppProperties;
 import Input.LocalSnapshotSource;
 import Model.SnapShot;
 import ClusteringGrid.GridDBSCANSTREAM;
-import PatternEnumerationGraph.EdgeGenerator;
-
-import PatternEnumerationGraph.PatternMiningGraph;
-import PatternEnumerationGraph.StarMiner;
 import PatternEnumerationGrid.CESMining;
 import PatternEnumerationGrid.ClusterEvolvingStream;
 import PatternEnumerationGrid.GridExtracting;
@@ -28,94 +24,90 @@ import scala.Tuple2;
 import java.util.*;
 
 public class MainAppGrid2 {
-    public static void main(String[] args) throws Exception {
-        // todo 1、得到环境参数和输入文件路径
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(1);
-//        env.setRestartStrategy(RestartStrategies.fixedDelayRestart( 3,// 尝试重启的次数
-//                org.apache.flink.api.common.time.Time.of(10, TimeUnit.SECONDS)));
+        public static void main(String[] args) throws Exception {
+                // todo 1、得到环境参数和输入文件路径
+                StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+                env.setParallelism(1);
 
-        String snapshotRootPath = AppProperties.getProperty("hdfs_input");
-        String hdfs_uri = AppProperties.getProperty("hdfs_uri");
-        int isConstrainTime = Integer.parseInt(AppProperties.getProperty("isConstrainTime"));
-        int StartNo = Integer.parseInt(AppProperties.getProperty("StartNo"));
-        int StopNo = Integer.parseInt(AppProperties.getProperty("StopNo"));
+                String snapshotRootPath = AppProperties.getProperty("hdfs_input");
+                String hdfs_uri = AppProperties.getProperty("hdfs_uri");
+                int isConstrainTime = Integer.parseInt(AppProperties.getProperty("isConstrainTime"));
+                int StartNo = Integer.parseInt(AppProperties.getProperty("StartNo"));
+                int StopNo = Integer.parseInt(AppProperties.getProperty("StopNo"));
 
-        int isTest = Integer.parseInt(AppProperties.getProperty("test"));
-        int testTime = AppProperties.getProperty("test_time") == null ? 0 : Integer.parseInt(AppProperties.getProperty("test_time"));
-        int parallelism = 1;
-        int partitionNum = 2;
-        int geoHashPrecision = 4;
+                int isTest = Integer.parseInt(AppProperties.getProperty("test"));
+                int testTime = AppProperties.getProperty("test_time") == null ? 0
+                                : Integer.parseInt(AppProperties.getProperty("test_time"));
+                int parallelism = 1;
+                int partitionNum = 2;
+                int geoHashPrecision = 4;
 
-        // todo 2、得到聚类参数并生成聚类结果
-        // 设置 DBSCAN 参数
-        double gridSize = Double.parseDouble(AppProperties.getProperty("gridSize"));
-        double ConversionFactor = Double.parseDouble(AppProperties.getProperty("conversion_factor"));
-        double epsilon = Double.parseDouble(AppProperties.getProperty("eps"));
-        int minPts = Integer.parseInt(AppProperties.getProperty("minPts"));
-        int M = Integer.parseInt(AppProperties.getProperty("M"));
-        int K = Integer.parseInt(AppProperties.getProperty("K"));
-        int L = Integer.parseInt(AppProperties.getProperty("L"));
-        int G = Integer.parseInt(AppProperties.getProperty("G"));
-        int SlidingWinndowSize = ((K/L)-1)*(G-1)+K+L-1;
-        int earth = Integer.parseInt(AppProperties.getProperty("earth"));
-        parallelism = Integer.parseInt(AppProperties.getProperty("parallelism"));       // 应该是2的倍数，但最好是2的次幂,至少是2，最好小于32
-        partitionNum = Integer.parseInt(AppProperties.getProperty("partitionNum"));
-        geoHashPrecision = Integer.parseInt(AppProperties.getProperty("geoHashPrecision"));
-        long TwindowSize = Long.parseLong(AppProperties.getProperty("TwindowSize"));
-        long TransGap = Long.parseLong(AppProperties.getProperty("TransGap"));
-        long MAX_RUNTIME = Long.parseLong(AppProperties.getProperty("MAX_RUNTIME"));
-        int miningTimes = Integer.parseInt(AppProperties.getProperty("miningTimes"));
-        DataStream<SnapShot> snapshotStream = env.addSource(new LocalSnapshotSource(hdfs_uri, snapshotRootPath, isTest, testTime, TransGap, isConstrainTime, StartNo, StopNo))
-                .assignTimestampsAndWatermarks(new MyTimestampExtractor());
+                // todo 2、得到聚类参数并生成聚类结果
+                // 设置 DBSCAN 参数
+                double gridSize = Double.parseDouble(AppProperties.getProperty("gridSize"));
+                double ConversionFactor = Double.parseDouble(AppProperties.getProperty("conversion_factor"));
+                double epsilon = Double.parseDouble(AppProperties.getProperty("eps"));
+                int minPts = Integer.parseInt(AppProperties.getProperty("minPts"));
+                int M = Integer.parseInt(AppProperties.getProperty("M"));
+                int K = Integer.parseInt(AppProperties.getProperty("K"));
+                int L = Integer.parseInt(AppProperties.getProperty("L"));
+                int G = Integer.parseInt(AppProperties.getProperty("G"));
+                int SlidingWinndowSize = ((K / L) - 1) * (G - 1) + K + L - 1;
+                int earth = Integer.parseInt(AppProperties.getProperty("earth"));
+                parallelism = Integer.parseInt(AppProperties.getProperty("parallelism")); // 应该是2的倍数，但最好是2的次幂,至少是2，最好小于32
+                partitionNum = Integer.parseInt(AppProperties.getProperty("partitionNum"));
+                geoHashPrecision = Integer.parseInt(AppProperties.getProperty("geoHashPrecision"));
+                long TwindowSize = Long.parseLong(AppProperties.getProperty("TwindowSize"));
+                long TransGap = Long.parseLong(AppProperties.getProperty("TransGap"));
+                long MAX_RUNTIME = Long.parseLong(AppProperties.getProperty("MAX_RUNTIME"));
+                int miningTimes = Integer.parseInt(AppProperties.getProperty("miningTimes"));
+                DataStream<SnapShot> snapshotStream = env
+                                .addSource(new LocalSnapshotSource(hdfs_uri, snapshotRootPath, isTest, testTime,
+                                                TransGap, isConstrainTime, StartNo, StopNo))
+                                .assignTimestampsAndWatermarks(new MyTimestampExtractor());
 
-        Long startTime = System.currentTimeMillis(); // 记录开始时间
-        // todo 2-1 基于KD-tree集中式的聚类方法
-//        DataStream<List<Partition>> globalPartitions = snapshotStream
-//                .map(new KdTreePartitioningFunction(partitionNum, epsilon, ConversionFactor));
-////        globalPartitions.print();
-//        DataStream<Set<Cluster>> localClustersStream = globalPartitions
-//                .map(new LocalDBSCANFunction(epsilon, minPts)).setParallelism(20);
-////        localClustersStream.print();
-//        DataStream<Set<Cluster>> GlobalClustersStream = localClustersStream
-//                .flatMap(new GlobalMergerFunction(M));
-        // todo 2-2 基于Grid-QUAD-tree的流式聚类方法
-        System.out.println("窗口大小为:"+SlidingWinndowSize+"--------------------------------------------------------------------------");
-        GridDBSCANSTREAM gDBSCAN = new GridDBSCANSTREAM();
-        DataStream<Cluster> clusterStream = gDBSCAN.gridDBSCAN_Launcher(snapshotStream, geoHashPrecision, M, epsilon, minPts, ConversionFactor, TwindowSize, parallelism, isTest, SlidingWinndowSize);
+                Long startTime = System.currentTimeMillis(); // 记录开始时间
+                // todo 2-1 基于KD-tree集中式的聚类方法
+                // DataStream<List<Partition>> globalPartitions = snapshotStream
+                // .map(new KdTreePartitioningFunction(partitionNum, epsilon,
+                // ConversionFactor));
+                //// globalPartitions.print();
+                // DataStream<Set<Cluster>> localClustersStream = globalPartitions
+                // .map(new LocalDBSCANFunction(epsilon, minPts)).setParallelism(20);
+                //// localClustersStream.print();
+                // DataStream<Set<Cluster>> GlobalClustersStream = localClustersStream
+                // .flatMap(new GlobalMergerFunction(M));
+                // todo 2-2 基于Grid-QUAD-tree的流式聚类方法
+                System.out.println("窗口大小为:" + SlidingWinndowSize
+                                + "--------------------------------------------------------------------------");
+                GridDBSCANSTREAM gDBSCAN = new GridDBSCANSTREAM();
+                DataStream<Cluster> clusterStream = gDBSCAN.gridDBSCAN_Launcher(snapshotStream, geoHashPrecision, M,
+                                epsilon, minPts, ConversionFactor, TwindowSize, parallelism, isTest,
+                                SlidingWinndowSize);
 
-        int finalGeoHashPrecision = geoHashPrecision;
-        DataStream<SubMap> ces = clusterStream
-//                .map(new MapFunction<Cluster, Cluster>() {
-//                    @Override
-//                    public Cluster map(Cluster cluster) throws Exception {
-////                        System.out.println(cluster.ptIDs);
-//                        return cluster;
-//                    }
-//                })
-                .keyBy(cluster -> cluster.getClusterID().substring(0, finalGeoHashPrecision))     // partition by ptid
-                .process(new GridExtracting(SlidingWinndowSize, M, K, L, G))
-                .setParallelism(parallelism)
-                ;
+                int finalGeoHashPrecision = geoHashPrecision;
+                DataStream<SubMap> ces = clusterStream
+                                .keyBy(cluster -> cluster.getClusterID().substring(0, finalGeoHashPrecision)) // partition
+                                                                                                              // by ptid
+                                .process(new GridExtracting(SlidingWinndowSize, M, K, L, G))
+                                .setParallelism(parallelism);
 
-        DataStream<Set<Set<Integer>>> patterns = ces
-                .flatMap(new CESMining(SlidingWinndowSize, M, K, L, G))
-                .setParallelism(parallelism)
-                ;
-        // 持久化的处理
-//        patterns.writeAsText("hdfs://amax:9000//result/result.txt").setParallelism(1);
+                DataStream<Set<Set<Integer>>> patterns = ces
+                                .flatMap(new CESMining(SlidingWinndowSize, M, K, L, G))
+                                .setParallelism(parallelism);
+                // 持久化的处理
+                // patterns.writeAsText("hdfs://amax:9000//result/result.txt").setParallelism(1);
 
-        env.execute("GridPatternMining");
-        Long endTime = System.currentTimeMillis();  // 记录结束时间
-        long totalTime = endTime - startTime;   // 计算总耗时
+                env.execute("GridPatternMining");
+                Long endTime = System.currentTimeMillis(); // 记录结束时间
+                long totalTime = endTime - startTime; // 计算总耗时
 
-        System.out.println("运行总耗时 " + totalTime + "ms");
+                System.out.println("运行总耗时 " + totalTime + "ms");
 
-    }
+        }
 
 }
 
-
-
 // hdfs dfs -put /data/ds/dataset/beijing-part /dataset/beijing-part
-// hdfs dfs -put /data/ds/dataset/Geolife/newPivotStream /dataset/Geolife/newPivotStream
+// hdfs dfs -put /data/ds/dataset/Geolife/newPivotStream
+// /dataset/Geolife/newPivotStream

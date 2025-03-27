15022a26bf2da4252f0677a842af1ad3a0d5dee8
Frankiewow <57431126+iam-frankie@users.noreply.github.com>
2025-03-27 13:59:14 +0800

    Update PartitionCalculator.java

--- src/main/java/Launch/PartitionCalculator.java
+++ src/main/java/Launch/PartitionCalculator.java
@@ -1,11 +1,6 @@
 package Launch;
 import org.apache.flink.api.common.functions.MapFunction;
 import Model.SnapShot;
-/*
-首先，我们需要创建一个函数来计算分区数。这个函数将根据数据量动态调整分区数。
-我们可以设定一个基础分区数，然后根据数据量的大小进行调整。
-例如，我们可以设定每1000个数据点为一个基础分区，然后根据数据量的大小进行调整。同时，我们需要确保分区数是2的倍数。
- */
 
 public class PartitionCalculator implements MapFunction<SnapShot, Integer> {
     private int basePartitionSize;
